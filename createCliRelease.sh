cecde0ec33b8fa21faf96e34f5d409d396457edf
David Rodriguez <david.rodriguez@williamhill.com>
2017-05-25 09:50:28 +0200

    Modify assemble to add the new starter script

--- createCliRelease.sh
+++ createCliRelease.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mvn install dependency:copy-dependencies -DoutputDirectory=lib
+mvn install dependency:copy-dependencies
 
 rm -rf runnable
 mkdir runnable
@@ -8,6 +8,6 @@ mkdir runnable
 cp -R cassandra-unit/src/main/cli/* runnable
 
 mkdir runnable/lib
-cp -R cassandra-unit/lib runnable/
+cp cassandra-unit/target/dependency/* runnable/lib/
 
 cp cassandra-unit/target/cassandra-unit-3.1.4.0-SNAPSHOT.jar runnable/lib/
